065a2c6e8d80341af8ad0f553f028305849e1b64
head <head@ipap.ru>
2018-12-19 13:25:02 +0300

    gunicorn

--- init.sh
+++ init.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+USER="web"
+# USER="firstuser"
+
+sudo ln -s /home/$USER/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
-#sudo /etc/init.d/gunicorn restart
+#sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
+sudo ln -s /home/$USER/etc/hello.py /etc/gunicorn.d/hello.py
+sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
