645063f92f90ddd88a8fa9cf2d396e67348dc341
head <head@ipap.ru>
2018-12-24 16:49:44 +0300

    fix init

--- init.sh
+++ init.sh
@@ -7,5 +7,6 @@ sudo ln -s /home/$USER/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 #sudo ln -s /home/box/web/etc/gunicorn.conf /etc/gunicorn.d/test
 sudo ln -s /home/$USER/web/etc/hello.py /etc/gunicorn.d/hello.py
+sudo ln -s /home/$USER/web/etc/django.py /etc/gunicorn.d/django.py
 sudo /etc/init.d/gunicorn restart
 #sudo /etc/init.d/mysql start
